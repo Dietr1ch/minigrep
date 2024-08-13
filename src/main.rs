233ee713101650e4be3c5fa7c89a0144f0f7a8c3
Dietrich Daroch <Dietrich@Daroch.me>
2024-08-13 16:30:37 -0600

    Logging: Add pretty_env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
 use clap::Parser;
 use std::fs;
 
@@ -12,7 +16,9 @@ struct Args {
 }
 
 fn main() {
+    pretty_env_logger::init();
     let args = Args::parse();
+    debug!("Args: {:?}", args);
 
     let text = fs::read_to_string(args.file_path).expect("Failed to read file");
     println!("Text:\n{}", text);
