151b32b90463e2357a89a8e88c26d1928062ee95
Dietrich Daroch <Dietrich@Daroch.me>
2024-08-18 13:28:35 -0600

    Perf: Add rayon support
    I'm also using ~Path~ instead of ~String~

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,26 @@ use color_print::cprintln;
 use regex::Regex;
 use std::error::Error;
 use std::fs;
+use std::path::Path;
 
-pub fn search_and_print_matches(
-    re: &Regex,
-    highlight: &str,
-    file_path: &str,
-) -> Result<(), Box<dyn Error>> {
-    debug!("Reading '{}'...", file_path);
+type StatusOr<T> = Result<T, Box<dyn Error>>;
+type Status = StatusOr<()>;
 
+pub fn search_and_print_matches(re: &Regex, highlight: &str, file_path: &Path) -> Status {
+    if !file_path.is_file() {
+        info!("Skipping directory '{:?}'...", file_path);
+        return Ok(());
+    }
+
+    debug!("Reading '{:?}'...", file_path);
     let text = fs::read_to_string(file_path)?;
+
     for (line_number, line) in text.lines().enumerate() {
         if re.is_match(line) {
             cprintln!(
-                "<green>{}</green>:<yellow>{}</yellow>| {}",
-                file_path,
+                "<green>{:?}/<bold>{:?}</bold></green>:<yellow>{}</yellow><bold>|</bold> {}",
+                file_path.parent(),
+                file_path.file_name(),
                 line_number,
                 re.replace_all(line, highlight)
             );
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ extern crate log;
 
 use clap::Parser;
 use color_print::cformat;
+use rayon::prelude::*;
 use regex::Regex;
+use std::path::Path;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,11 +15,52 @@ struct Args {
     query: String,
 
     #[arg()]
-    file_paths: Vec<String>,
+    file_paths: Vec<Box<Path>>,
 
     #[arg(long)]
     keep_going: bool,
+
+    #[arg(long)]
+    use_rayon: bool,
 }
+
+#[inline(never)]
+fn rayon(args: &Args, re: &Regex, highlight: &str) {
+    args.file_paths.par_iter().for_each(|file_path| {
+        match minigrep::search_and_print_matches(re, highlight, file_path) {
+            Ok(()) => {}
+            Err(e) => {
+                error!("Failed to read file '{}': {}", file_path.display(), e);
+                if !args.keep_going {
+                    panic!(
+                        "\nFailed to read file '{}'. Use `--keep_going` to be lenient on errors.",
+                        file_path.display()
+                    );
+                }
+            }
+        }
+    });
+}
+
+#[inline(always)]
+fn single_thread(args: &Args, re: &Regex, highlight: &str) {
+    for file_path in &args.file_paths {
+        match minigrep::search_and_print_matches(re, highlight, file_path) {
+            Ok(()) => {}
+            Err(e) => {
+                error!("Failed to read file '{}': {}", file_path.display(), e);
+                if !args.keep_going {
+                    panic!(
+                        "\nFailed to read file '{}'. Use `--keep_going` to be lenient on errors.",
+                        file_path.display()
+                    );
+                }
+            }
+        }
+    }
+}
+
+const MAX_SINGLE_THREAD_FILES: usize = 50;
 
 fn main() {
     pretty_env_logger::init();
@@ -27,20 +70,10 @@ fn main() {
     let highlight = cformat!("<bold><red>{}</red></bold>", &args.query);
     let re = Regex::new(&args.query).unwrap();
 
-    for file_path in args.file_paths {
-        match minigrep::search_and_print_matches(&re, &highlight, &file_path) {
-            Ok(()) => {
-                continue;
-            }
-            Err(e) => {
-                error!("Failed to read file '{}': {}", file_path, e);
-                if !args.keep_going {
-                    panic!(
-                        "\nFailed to read file '{}'. Use `--keep_going` to be lenient on errors.",
-                        file_path
-                    );
-                }
-            }
-        }
+    let use_rayon = args.use_rayon || args.file_paths.len() > MAX_SINGLE_THREAD_FILES;
+    if !use_rayon {
+        single_thread(&args, &re, &highlight);
+    } else {
+        rayon(&args, &re, &highlight);
     }
 }
